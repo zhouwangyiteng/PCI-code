928a75ed0b1c6975e7c88f7d121adfdf9e1a2831
zhouwangyiteng <zhouwang122@qq.com>
2015-12-21 21:56:42 +0800

    add the code before P40

--- chapter3/clusters.py
+++ chapter3/clusters.py
@@ -43,3 +43,80 @@ def pearson(v1, v2):
         return 0
     return 1.0 - num / den
 
+
+def hcluster(rows, distance=pearson):
+    distances = {}
+    currentclustid = -1
+
+    clust = [bicluster(rows[i], ID=i) for i in range(len(rows))]
+
+    while len(clust)>1:
+        lowestpair = (0, 1)
+        closest = distance(clust[0].vec, clust[1].vec)
+
+        for i in range(len(clust)):
+            for j in range(i+1, len(clust)):
+                if (clust[i].ID, clust[j].ID) not in distances:
+                    distances[(clust[i].ID, clust[j].ID)] = distance(clust[i].vec, clust[j].vec)
+                d = distances[(clust[i].ID, clust[j].ID)]
+                if d<closest:
+                    closest = d
+                    lowestpair = (i, j)
+
+        mergevec = [(clust[lowestpair[0]].vec[i] + clust[lowestpair[1]].vec[i]) / 2.0
+                    for i in range(len(clust[0].vec))]
+        newcluster = bicluster(mergevec, left=clust[lowestpair[0]],
+                               right=clust[lowestpair[1]],
+                               distance=closest, ID=currentclustid)
+
+        currentclustid -= 1
+        del clust[lowestpair[1]]
+        del clust[lowestpair[0]]
+        clust.append(newcluster)
+
+    return clust[0]
+
+def printclust(clust, labels=None, n=0):
+    for i in range(n):
+        print ' ',
+    if clust.ID<0:
+        print '-'
+    else:
+        if labels==None:
+            print clust.ID
+        else:
+            print labels[clust.ID]
+
+    if clust.left!=None:
+        printclust(clust.left, labels=labels, n=n+1)
+    if clust.right!=None:
+        printclust(clust.right, labels=labels, n=n+1)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
