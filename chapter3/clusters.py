459ce3b884185d02d89bb2c63f09935b504b223f
zhouwangyiteng <zhouwang122@qq.com>
2015-12-22 20:20:46 +0800

    add the code before P45

--- chapter3/clusters.py
+++ chapter3/clusters.py
@@ -1,5 +1,6 @@
 from math import sqrt
 from PIL import Image, ImageDraw
+import random
 
 path = r'F:\pycode\PCI\PCI_data\\'
 
@@ -143,11 +144,48 @@ def drawnode(draw, clust, x, y, scaling, labels):
         draw.text((x+5, y-7), labels[clust.ID], (0,0,0))
         
 
+def kcluster(rows, distance=pearson, k=4):
+    ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows]))
+              for i in range(len(rows[0]))]
+
+    clusters = [[random.random()*(ranges[i][1] - ranges[1][0]) + ranges[i][0]
+                 for i in range(len(rows[0]))] for j in range(k)]
+
+    lastmatches = None
+    for t in range(100):
+        print 'Iteration %d' % t
+        bestmatches = [[] for i in range(k)]
+
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatch = 0
+            for i in range(k):
+                d = distance(clusters[i], row)
+                if d<distance(clusters[bestmatch], row):
+                    bestmatch = i
+            bestmatches[bestmatch].append(j)
+
+        if bestmatches==lastmatches:
+            break
+        lastmatches = bestmatches
+
+        for i in range(k):
+            avgs = [0.0] * len(rows[0])
+            if len(bestmatches[0])>0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m] += rows[rowid][m]
+                for j in range(len(avgs)):
+                    avgs[j] /= len(bestmatches[i])
+                clusters[i] = avgs
+    return bestmatches
+
+
+
+
 
 blognames, words, data = readfile('blogdata.txt')
-clust = hcluster(data)
-drawdendrogram(clust, blognames)
-
+kclust = kcluster(data, k=10)
 
 
 
